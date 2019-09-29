884f45d44bb871f63c06f22fbfca01f4359bac44
Andrew <andrewchess25@gmail.com>
2019-09-29 15:08:20 +0300

    Contact, Country and City models were added; models migrations were executed.

---
+++ address_book/database/migrations/2019_09_29_114745_create_contacts_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateContactsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('contacts', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->string('email');
+            $table->unsignedBigInteger('country_id');
+            $table->unsignedBigInteger('city_id');
+            $table->string('photo');
+            $table->text('description');
+            $table->timestamps();
+
+            $table->index('country_id');
+            $table->index('city_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('contacts');
+    }
+}
