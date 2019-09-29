804d8734da2f6fd79c03cd8c9405e9f3393d7599
Andrew <andrewchess25@gmail.com>
2019-09-29 15:16:46 +0300

    Relation between user contact and city was setup; relation between user contact and country was setup.

--- address_book/app/Contact.php
+++ address_book/app/Contact.php
@@ -6,5 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Contact extends Model
 {
-    //
+    public function city(){
+        return $this->belongsTo(City::class);
+    }
+
+    public function country(){
+        return $this->belongsTo(Country::class);
+    }
 }
