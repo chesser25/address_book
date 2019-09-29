bff5e77e897e9d20e93e20c7382fa2218f123d4a
Andrew <andrewchess25@gmail.com>
2019-09-29 16:25:25 +0300

    Search table was added on the home view.

--- address_book/resources/views/home.blade.php
+++ address_book/resources/views/home.blade.php
@@ -2,22 +2,45 @@
 
 @section('content')
 <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Dashboard</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    You are logged in!
+    <table class="table table-bordered">
+        <thead>
+          <tr>
+            <th class="text-center" colspan="4">
+                <h3>Search</h3>
+            </th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td class="d-flex">
+                <div class="col-4 d-flex h-100 justify-content-center align-items-center">
+                    <div class="col-3">Keywords: </div>
+                    <div class="col-6">
+                        <input name="keywords" id="keywords"/>
+                    </div>
                 </div>
-            </div>
-        </div>
-    </div>
+                <div class="col-4 d-flex h-100 justify-content-center align-items-center">
+                    <div class="col-3">Country: </div>
+                    <div class="col-6">
+                        <select class="form-control" name="country_id">
+                          <option value="Ukraine">Ukraine</option>
+                      </select>
+                    </div>
+                </div>
+                <div class="col-4 d-flex h-100 justify-content-center align-items-center">
+                    <div class="col-3">City: </div>
+                    <div class="col-6">
+                        <select class="form-control" name="city_id">
+                            <option value="Kirovograd">Kirovograd</option>
+                        </select>
+                    </div>
+                </div>
+            </td>
+          </tr>
+          <tr>
+            <td class="d-flex justify-content-center"><button class="btn btn-primary">Search</button></td>
+          </tr>
+        </tbody>
+      </table>
 </div>
 @endsection
