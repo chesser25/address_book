1cf78611499ce2bd4645553676dcd1fb5fabd6f1
Andrew <andrewchess25@gmail.com>
2019-09-29 18:26:21 +0300

    Routes for Contact controller were added (Get and Post).

--- address_book/routes/web.php
+++ address_book/routes/web.php
@@ -18,3 +18,5 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/contact/create', 'ContactsController@create')->name('contact.create');
+Route::post('/contact', 'ContactsController@store')->name('contact.store');
