26f9615adc09efaec603ceba441b5d56e324bf30
Andrew <andrewchess25@gmail.com>
2019-09-30 11:04:18 +0300

    Get method was setup to send lists of cities & countries to the view. Post method was setup to validate & save data.

--- address_book/app/Http/Controllers/ContactsController.php
+++ address_book/app/Http/Controllers/ContactsController.php
@@ -3,11 +3,28 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use \App\Country;
+use \App\City;
 
 class ContactsController extends Controller
 {
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
     public function create(){
-        return view('contacts/create');
+        $countries = Country::all();
+        $cities = City::all();
+        return view('contacts/create',[
+            'countries' => $countries,
+            'cities' => $cities
+        ]);
     }
 
     public function store(){
@@ -20,6 +37,17 @@ class ContactsController extends Controller
             'photo' => ['required', 'image'],
             'description' => ''
         ]);
-        dd(request()->all());
+        $country = \App\Country::where('name', $data['country'])->firstOrFail();
+        $city = \App\City::where('name', $data['city'])->firstOrFail();
+        $contact = new \App\Contact();
+        $contact->first_name = $data['first_name'];
+        $contact->last_name = $data['last_name'];
+        $contact->email = $data['email'];
+        $contact->photo = $data['photo'];
+        $contact->description = $data['description'];
+        $contact->country_id = $country;
+        $contact->city_id = $city;
+        $contact->save();
+        return view('/home');
     }
 }
