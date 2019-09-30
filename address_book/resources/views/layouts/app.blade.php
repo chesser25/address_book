0307880bb7b2fc4b0de9967f2f903a66e82f12ae
Andrew <andrewchess25@gmail.com>
2019-09-30 23:37:15 +0300

    app.js file was included in layout.

--- address_book/resources/views/layouts/app.blade.php
+++ address_book/resources/views/layouts/app.blade.php
@@ -9,6 +9,8 @@
 
     <title>Address Book</title>
 
+    <script src="{{ asset('js/app.js') }}"></script>
+
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
