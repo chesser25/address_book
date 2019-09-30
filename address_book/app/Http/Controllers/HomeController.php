84b1b27c41abd17b49135ef6ada252ecd81c8637
Andrew <andrewchess25@gmail.com>
2019-10-01 01:23:43 +0300

    Logic to get a list of contacts was added.

--- address_book/app/Http/Controllers/HomeController.php
+++ address_book/app/Http/Controllers/HomeController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use App\Contact;
 
 class HomeController extends Controller
 {
@@ -23,6 +25,8 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home');
+        $contactsPerPage = 1;
+        $contacts = Contact::paginate($contactsPerPage);
+        return view('home', compact('contacts'));
     }
 }
