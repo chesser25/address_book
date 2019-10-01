135b347f4d604366efb828a78e1430e7c1887749
Andrew <chess2508@mail.ru>
2019-10-01 20:30:40 +0300

    Routes sequence was fixed.

--- address_book/routes/web.php
+++ address_book/routes/web.php
@@ -15,10 +15,14 @@ Route::get('/', 'ContactsController@index');
 
 Auth::routes();
 
-Route::get('/dashboard', 'ContactsController@index')->middleware('auth');
-Route::get('/contact/{person}/edit', 'ContactsController@edit')->middleware('auth');
-Route::patch('/contact/{person}', 'ContactsController@update')->middleware('auth');
 Route::get('/contact/create', 'ContactsController@create')->middleware('auth');
 Route::post('/contact', 'ContactsController@store')->middleware('auth');
-Route::delete('/contact/{person}/destroy', 'ContactsController@destroy')->middleware('auth');;
+
+Route::get('contact/{person}', 'ContactsController@show')->name('contact.show');
+Route::get('/dashboard', 'ContactsController@index')->middleware('auth');
+
+Route::get('/contact/{person}/edit', 'ContactsController@edit')->middleware('auth');
+Route::patch('/contact/{person}', 'ContactsController@update')->middleware('auth');
+
+Route::delete('/contact/{person}/destroy', 'ContactsController@destroy')->middleware('auth');
 
